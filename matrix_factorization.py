581fa91aa19b59995743adef2fc765ff4c102797
keya-desai <keyadesai97@gmail.com>
2020-04-13 15:03:21 -0400

    Split code into two files

--- matrix_factorization.py
+++ matrix_factorization.py
@@ -1,34 +1,43 @@
 import pandas as pd
 import numpy as np
 from surprise import Reader, Dataset
-from surprise.model_selection import train_test_split
+from surprise import SVD, accuracy, SVDpp, SlopeOne, BaselineOnly, CoClustering
 from surprise import SVD, accuracy
-
-# ratings matrix
-df_ratings = pd.read_csv('ml-latest/ratings.csv')
-# print(df_ratings.head())
-
-# print(df_ratings.rating.value_counts())
-
-data = df_ratings.iloc[:, :-1]
-print(data.head())
-
-# load dataset
-print("Reading data")
-reader = Reader()
-data = Dataset.load_from_df(data[['userId', 'movieId', 'rating']], reader)
+import data
 
 
-# split dataset into train and test
-trainset, testset = train_test_split(data, test_size=0.20)
+def convert_traintest_dataframe_forsurprise(training_dataframe, testing_dataframe):
+    training_dataframe = training_dataframe.iloc[:, :-1]
+    testing_dataframe = testing_dataframe.iloc[:, :-1]
+    reader = Reader(rating_scale=(0,5))
+    trainset = Dataset.load_from_df(training_dataframe[['userId', 'movieId', 'rating']], reader)
+    testset = Dataset.load_from_df(testing_dataframe[['userId', 'movieId', 'rating']], reader)
+    trainset = trainset.construct_trainset(trainset.raw_ratings)
+    testset=testset.construct_testset(testset.raw_ratings)
+    return([trainset,testset])
 
-print("Running the algorithm")
-algo = SVD()
-algo.fit(trainset)
+def svdalgorithm(trainset, testset):
+    algo = SVD()
+    algo.fit(trainset)
+    print("Predictions")
+    predictions = algo.test(testset)
+    accuracy.rmse(predictions)
+    accuracy.mae(predictions)
 
-print("Predictions")
-predictions = algo.test(testset)
+def baseline(trainset, testset):
+    algo = BaselineOnly()
+    algo.fit(trainset)
+    print("Predictions")
+    predictions = algo.test(testset)
+    accuracy.rmse(predictions)
+    accuracy.mae(predictions)
 
 
-# evaluation
-print("accuracy = ", accuracy.rmse(predictions))
+if __name__ == "__main__":
+
+	df_train, df_test = data.get_train_test_data()
+	trainset, testset = convert_traintest_dataframe_forsurprise(df_train, df_test)
+	print("Baseline algorithm using surprise package")
+	baseline(trainset, testset)
+	print("SVD algorithm using surprise package")
+	svdalgorithm(trainset,testset)
